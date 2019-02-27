4a84bc6534ff569090cf3d442a2c87de4621981e
anupama bhimnath <37138428+anupamabhimnath@users.noreply.github.com>
2019-02-27 14:41:26 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    $("#myform").submit(function(){
+        var search = $("#books").val();
+        if(search == '')
+        {
+            alert('Please enter something in the given field');
+        }
+        else
+        {
+            var url ='';
+            var img = '';
+            var author = '';
+            var title = '';
+            
+            $.get("https://www.googleapis.com/books/v1/volumes/?q=" + search,function(response){
+
+                for(var i=0;i<response.items.length;i++)
+                {
+                    //get the title of books
+                    title= $('<h5 class="center-align black-text">' + response.items[i].volumeInfo.title + '</h5>');
+
+                    author= $('<h5 class="center-align black-text">' + response.items[i].volumeInfo.authors + '</h5>');
+
+                    img = $('<img class = "aligning card z-depth-5" id = "dynamic" <br> < a href=' + response.items[i].volumeInfo.infoLink + '> <button id = "imagebutton" class =" btn red aligning">Read More</button></a>'); 
+ 
+                    url = response.items[i].volumeInfo.imageLinks.thumbnail;
+
+                    img.attr('src', url); // attach the image url
+
+                    document.getElementById("search").innerHTML += "<br>" + item.volumeInfo.title;
+
+                    title.appendTo("#result");
+
+                    author.appendTo("#result");
+
+                    img.appendTo("#result");
+                }
+            });
+
+            $.get("https://www.googleapis.com/books/v1/volumes?q= search & callback=handleResponse");
+
+        }
+        var settings = {
+            "url": "https://github.com/anupamabhimnath/GoogleBooksAPI.git",
+            "method": "GET",
+            "timeout": 0,
+          };
+          
+          $.ajax(settings).done(function (response) {
+            console.log(response);
+          });
+    });
+return false;
+});
+
+
